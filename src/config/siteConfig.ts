8ae5c5902c774f3fe787f34c02308fbe51707bdd
wind <boltjolt@qq.com>
2026-09-07 11:08:35 +0800

    改变图片

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -90,7 +90,7 @@ export const siteConfig: SiteConfig = {
   favicon: [
     {
       // 图标文件路径
-      src: "/favicon/firefly-32.png",
+      src: "/favicon/logo.png",
       // 可选，指定主题 'light' | 'dark'
       // theme: "light",
       // 可选，图标大小
@@ -111,8 +111,8 @@ export const siteConfig: SiteConfig = {
     // 使用 Astro 图标库时不需要设置 valueDark，图标会自动跟随主题亮暗色切换
     logo: {
       type: "image",
-      value: "assets/images/logo/firefly-light.png",
-      valueDark: "assets/images/logo/firefly-dark.png",
+      value: "assets/images/logo/logo-light.png",
+      valueDark: "assets/images/logo/logo-light.png",
       alt: "🍀",
     },
     // 导航栏标题
